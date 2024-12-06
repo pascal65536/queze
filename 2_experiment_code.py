efb2920b297f7f96b5c026edd0304b59a4ebb64a
Sergey Pakhtusov <pascal65536@gmail.com>
2024-12-06 17:18:12 +0700

    1_experiment

---
+++ 2_experiment_code.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt6 import uic
+
+from PyQt6.QtWidgets import QApplication, QWidget
+
+
+class Calculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("2_experiment.ui", self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Calculator()
+    ex.show()
+    sys.exit(app.exec())
